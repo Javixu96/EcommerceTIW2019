66d1e7942772d93ca4323127517e7349c5766429
sensenye <100349203@alumnos.uc3m.es>
2019-12-05 14:29:39 +0100

    Configuracion Spring + Entidades

---
+++ EcommerceUsersMS/src/main/java/es/uc3m/ecommerce/users/model/PurchasDAO.java
@@ -0,0 +1,18 @@
+package es.uc3m.ecommerce.users.model;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import es.uc3m.ecommerce.users.model.Appuser;
+
+public interface PurchasDAO extends CrudRepository<Purchas, Long>{
+
+	@Query("SELECT DISTINCT p.confirmationCode FROM Purchas p where p.appuser.userId = :userId")
+	public List<Integer> findAllConfirmationCode(int userId);
+	
+	public List<Purchas> findByConfirmationCode(int confirmationCode);
+	
+	public Appuser save(Appuser puser);
+}
