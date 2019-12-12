3a1a9971cf7081959b47f960ff77e1ca94362b99
Javixu96 <1996javixu@gmail.com>
2019-12-12 10:52:28 +0100

    integrando

--- EcommerceUsers/src/es/uc3m/ecommerce/controller/LoginRequestHandler.java
+++ EcommerceUsers/src/es/uc3m/ecommerce/controller/LoginRequestHandler.java
@@ -22,17 +22,29 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 import javax.transaction.UserTransaction;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import es.uc3m.ecommerce.manager.UserManager;
 import es.uc3m.ecommerce.model.*;
 
 /*
 * Handler que se encarga del login del usuario
 */
 public class LoginRequestHandler implements IHandler {
+	Client client;
+	WebTarget webTarget;
+	WebTarget webTargetPath;
+	Invocation.Builder invocationBuilder;
+	Response resp;
 	
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+			
+			client = ClientBuilder.newClient();
+			webTarget = client.target("http://localhost:13101");
 			String viewURL = null; 
 			HttpSession session = request.getSession();
 
@@ -48,13 +60,17 @@ public class LoginRequestHandler implements IHandler {
 				// Tomo los datos del formulario de log in 
 				String introducedEmail  = request.getParameter("register_email");
 				String introducedPassword = request.getParameter("register_password");
-
+				List<Appuser> userListByName=new ArrayList<Appuser>();
+				WebTarget webTargetPath = webTarget
+						.path("users/login").queryParam("email",introducedEmail).queryParam("pw", introducedPassword);
+				invocationBuilder = webTargetPath.request(MediaType.APPLICATION_JSON);
+				Response responsews = invocationBuilder.get();
 				
-				// Busco en BD una objeto usuario que tenga el mismo email que el introducido
-				UserManager manager = new UserManager();
+				Appuser[] allUsers=responsews.readEntity(Appuser[].class);
+				for(int i=0;i<allUsers.length;i++) {
+					userListByName.add(allUsers[i]);
+				}
 				
-				List<Appuser> userListByName = manager.findByEmail(introducedEmail);
-				System.out.println("he size of the list of users under that email: " + userListByName.size());
 				// Si no existe ese email en BD, el usuario no esta registrado y no puede hacer log in
 				if(userListByName.size() != 0) {
 					
--- EcommerceUsers/src/es/uc3m/ecommerce/controller/RegisterRequestHandler.java
+++ EcommerceUsers/src/es/uc3m/ecommerce/controller/RegisterRequestHandler.java
@@ -1,6 +1,7 @@
 package es.uc3m.ecommerce.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.naming.Context;
@@ -14,14 +15,25 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.transaction.UserTransaction;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import es.uc3m.ecommerce.manager.UserManager;
 import es.uc3m.ecommerce.model.Appuser;
 
 /*
 * Handler para registrar un nuevo usuario comprador / vendedor
 */
 public class RegisterRequestHandler implements IHandler {
+	Client client;
+	WebTarget webTarget;
+	WebTarget webTargetPath;
+	Invocation.Builder invocationBuilder;
+	Response resp;
 	
 	private EntityManagerFactory emf;
 	private UserTransaction ut;
@@ -29,6 +41,10 @@ public class RegisterRequestHandler implements IHandler {
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		client = ClientBuilder.newClient();
+		webTarget = client.target("http://localhost:13101");
+		WebTarget webTargetPath;
+		Response responsews;
 		String viewURL = null;
 
 		// Tomo los parametros del form de register
@@ -40,7 +56,17 @@ public class RegisterRequestHandler implements IHandler {
 		int introducedRole = Integer.parseInt(request.getParameter("register_role"));
 			
 		// Retorno la lista de usuarios que tienen ese email (deber ser de tamaño 0 o 1)
-		List<Appuser> userListByName = manager.findByEmail(introducedEmail);
+		
+		List<Appuser> userListByName=new ArrayList<Appuser>();
+		webTargetPath = webTarget
+				.path("users/login").queryParam("email",introducedEmail);
+		invocationBuilder = webTargetPath.request(MediaType.APPLICATION_JSON);
+		responsews = invocationBuilder.get();
+		
+		Appuser[] allUsers=responsews.readEntity(Appuser[].class);
+		for(int i=0;i<allUsers.length;i++) {
+			userListByName.add(allUsers[i]);
+		}
 		System.out.println("UserListByName returned by manager when checking if email already registered is size " + userListByName.size());
 		
 		// Si el email ya esta registrado
@@ -50,8 +76,20 @@ public class RegisterRequestHandler implements IHandler {
 			viewURL = "register.jsp";
 		// Si el email no estÃ¡ registrado
 		} else {
+			
 			// El id se genera automaticamente cuando se pone una nueva tupla en la tabla 
-			manager.insert(introducedName, introducedSurname, introducedAddress, introducedEmail, introducedPassword, introducedRole);
+			Appuser user=new Appuser();
+			user.setEmail(introducedEmail);
+			user.setPostalAddress(introducedAddress);
+			user.setPw(introducedPassword);
+			user.setUserName(introducedName);
+			user.setUserSurnames(introducedSurname);
+			user.setUserRole(introducedRole);
+			user.setIsDeleted(0);
+			webTargetPath = webTarget
+					.path("users/login").queryParam("email",introducedEmail);
+			invocationBuilder = webTargetPath.request(MediaType.APPLICATION_JSON);
+			responsews = invocationBuilder.post(Entity.entity(user,MediaType.APPLICATION_JSON));
 			
 			// Registro completado con exito -> redirijo al login con un mensaje basado en un atributo de request
 			request.setAttribute("registrationSuccess", 1);
